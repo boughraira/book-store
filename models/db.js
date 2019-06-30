5bd14e36c36a19f005184a3b97b348db4ecd0c0a
hazem <42295584+boughraira@users.noreply.github.com>
2019-06-30 16:25:09 +0100

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb+srv://hazem:hazem123@cluster0-o0rso.mongodb.net/test?retryWrites=true', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb://localhost:27017/livre', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
